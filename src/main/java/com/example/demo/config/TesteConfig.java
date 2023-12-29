edb4dd551e21ad3becc2cee89fe5b1da0df95b84
João Paulo Cunha <joaopaulofaria98@gmail.com>
2023-12-29 10:54:01 -0300

    Payment One-To-one association

--- src/main/java/com/example/demo/config/TesteConfig.java
+++ src/main/java/com/example/demo/config/TesteConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.example.demo.entites.Category;
 import com.example.demo.entites.Order;
 import com.example.demo.entites.OrderItem;
+import com.example.demo.entites.Payment;
 import com.example.demo.entites.Product;
 import com.example.demo.entites.User;
 import com.example.demo.entites.enums.OrderStatus;
@@ -79,6 +80,11 @@ public class TesteConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
         
 		orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
+
+		
+		Payment py1 = new Payment(null,Instant.parse("2019-06-20T21:53:07Z"),o1);
+	    o1.setPayment(py1);
+		orderRepository.save(o1);
 	}
 
 }
--- src/main/java/com/example/demo/entites/Order.java
+++ src/main/java/com/example/demo/entites/Order.java
@@ -9,6 +9,7 @@ import java.util.Set;
 import com.example.demo.entites.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -16,6 +17,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -36,6 +38,9 @@ public class Order implements Serializable {
 
 	@OneToMany(mappedBy = "id.order")
 	private Set<OrderItem> items = new HashSet<>();
+
+	@OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
+	private Payment payment;
 	
 	public Order() {
 	}
@@ -86,6 +91,15 @@ public class Order implements Serializable {
 		return items;
 	}
 	
+	
+	public Payment getPayment() {
+		return payment;
+	}
+
+	public void setPayment(Payment payment) {
+		this.payment = payment;
+	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
